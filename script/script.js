1a46c6f1d3b1b16c2129b3fdfd0b6c030d32ee61
TriforceParas <paras.tloz@gmail.com>
2024-10-03 21:42:48 +0530

    better file management

--- script/script.js
+++ script/script.js
@@ -1,10 +1,10 @@
-document.getElementById('loginForm')?.addEventListener('submit', function(e) {
+document.getElementById('loginForm')?.addEventListener('submit', function (e) {
     e.preventDefault();
     // Redirect to landing page
     window.location.href = 'landing.html';
 });
 
-document.getElementById('signupForm')?.addEventListener('submit', function(e) {
+document.getElementById('signupForm')?.addEventListener('submit', function (e) {
     e.preventDefault();
     alert('Signup functionality to be implemented.');
 });
